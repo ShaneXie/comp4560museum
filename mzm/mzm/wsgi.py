42cfa654852d495e567c3e9410150bd1443c958f
ShaneXie <shane369@gmail.com>
2016-09-21 07:23:26 -0500

    init django

---
+++ mzm/mzm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mzm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mzm.settings")
+
+application = get_wsgi_application()
---
+++ mzm/mzm_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MzmApiConfig(AppConfig):
+    name = 'mzm_api'
