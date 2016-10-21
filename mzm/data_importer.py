1bf80665efa678809ba20ebfddd18eddffcaaeed
ShaneXie <shane369@gmail.com>
2016-10-21 03:45:03 -0500

    wip fixing errors

--- mzm/data_importer.py
+++ mzm/data_importer.py
@@ -2,6 +2,7 @@
 import sys, os, django
 import xlrd
 import dotenv
+from django.utils import timezone
 
 def intTryParse(value):
     try:
@@ -21,6 +22,7 @@ if __name__ == '__main__':
 
     # if not os.path.exists(sys.argv[2]):
     #     sys.exit('ERROR: file %s was not found!' % sys.argv[1])
+    timezone.now()
 
     book = xlrd.open_workbook("./data_seed/UMZM migratory bird specimens_Feb2015.xls")
     sh = book.sheet_by_index(0)
@@ -28,14 +30,23 @@ if __name__ == '__main__':
     for r in range(sh.nrows)[2:]:
         data = sh.row(r)
         print(r)
+        print(data[8])
         # print(xlrd.xldate.xldate_as_datetime(data[8].value, 0))
         taxon = Taxon.objects.create(family=data[3].value, genus=data[5].value,
                                      specificEpithet=data[6].value, acceptedNameUsage=data[4].value)
         occurrence = Occurrence.objects.create(catalogNumber=data[7].value,
                                                sex=data[11].value, lifeStage = str(data[12].value))
         location = Location.objects.create(stateProvince=data[9].value, locality=data[10].value)
-        event = Event.objects.create(eventDate=xlrd.xldate.xldate_as_datetime(data[8].value, 1))
-        identification = Identification.objects.create(identificationID=int(data[0].value))
+        if data[8].ctype != xlrd.XL_CELL_EMPTY:
+            event = Event.objects.create(eventDate=timezone.make_aware(xlrd.xldate.xldate_as_datetime(data[8].value, 0),
+                                                                       timezone.get_current_timezone()))
+        else:
+            event = None
+
+        if data[0].ctype != xlrd.XL_CELL_EMPTY:
+            identification = Identification.objects.create(identificationID=int(data[0].value))
+        else:
+            identification = None
         root = Root(
             eventID=event,
             locationID = location,
@@ -44,5 +55,5 @@ if __name__ == '__main__':
             occurrenceID = occurrence,
             type = data[2].value
         )
-        root.save()
+        #root.save()
     print("data saved to database.")
