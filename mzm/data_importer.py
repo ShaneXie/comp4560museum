55ec38c7da423f59242eb2d8a48c7e802fc27c07
ShaneXie <shane369@gmail.com>
2016-10-25 21:56:31 -0500

    done bird importer

--- mzm/data_importer.py
+++ mzm/data_importer.py
@@ -3,6 +3,7 @@ import sys, os, django
 import xlrd
 import dotenv
 from django.utils import timezone
+from datetime import datetime
 
 def intTryParse(value):
     try:
@@ -17,29 +18,41 @@ if __name__ == '__main__':
     django.setup()
 
     from mzm_api.models import Taxon, Occurrence, Location, Event, Root, Identification
-    # if len(sys.argv) < 3:
-    #     sys.exit('Usage: %s database-name file-name' % sys.argv[0])
 
-    # if not os.path.exists(sys.argv[2]):
-    #     sys.exit('ERROR: file %s was not found!' % sys.argv[1])
     timezone.now()
 
-    book = xlrd.open_workbook("./data_seed/UMZM migratory bird specimens_Feb2015.xls")
+    book = xlrd.open_workbook("./data_seed/UMZM migratory bird specimens_Feb2015-Converted.xls")
     sh = book.sheet_by_index(0)
+
+    print('Start importing data...')
+
     print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
     for r in range(sh.nrows)[2:]:
         data = sh.row(r)
-        print(r)
-        print(data[8])
-        # print(xlrd.xldate.xldate_as_datetime(data[8].value, 0))
+
         taxon = Taxon.objects.create(family=data[3].value, genus=data[5].value,
                                      specificEpithet=data[6].value, acceptedNameUsage=data[4].value)
         occurrence = Occurrence.objects.create(catalogNumber=data[7].value,
                                                sex=data[11].value, lifeStage = str(data[12].value))
         location = Location.objects.create(stateProvince=data[9].value, locality=data[10].value)
+
         if data[8].ctype != xlrd.XL_CELL_EMPTY:
-            event = Event.objects.create(eventDate=timezone.make_aware(xlrd.xldate.xldate_as_datetime(data[8].value, 0),
-                                                                       timezone.get_current_timezone()))
+            date = None
+            if data[8].ctype == xlrd.XL_CELL_TEXT:
+                dash_count = data[8].value.count('-')
+                slash_count = data[8].value.count('/')
+                if dash_count == 0:
+                    if slash_count == 0:
+                        date = datetime.strptime(data[8].value, "%Y")
+                    else:
+                        date = datetime.strptime(data[8].value, "%m/%d/%Y")
+                elif dash_count == 1:
+                    date = datetime.strptime(data[8].value, "%b-%Y")
+                else:
+                    date = datetime.strptime(data[8].value, "%d-%b-%Y")
+            else:
+                date = xlrd.xldate.xldate_as_datetime(data[8].value, 0)
+            event = Event.objects.create(eventDate=timezone.make_aware(date, timezone.get_current_timezone()))
         else:
             event = None
 
@@ -56,4 +69,4 @@ if __name__ == '__main__':
             type = data[2].value
         )
         root.save()
-    print("data saved to database.")
+    print("all bird data imported into database.")
