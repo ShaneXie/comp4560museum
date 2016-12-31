290ba7ac61f6c3ee67e5b2c26821cb3081113325
rickd <rickd@rickds-iMac.local>
2016-12-30 20:44:03 -0600

    start working on list

--- mzm-mobile/app/app.module.ts
+++ mzm-mobile/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+
 import { NativeScriptModule } from "nativescript-angular/platform";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { SIDEDRAWER_DIRECTIVES } from "nativescript-telerik-ui-pro/sidedrawer/angular";
 import { LISTVIEW_DIRECTIVES } from 'nativescript-telerik-ui-pro/listview/angular';
+
 import { AppComponent } from "./app.component";
 import {DrawerComponent} from "./pages/drawer/drawer.component";
 import {HomeComponent} from "./pages/home/home.component";
-
+import {AboutComponent} from "./pages/about/about.component";
 import {routes} from './app.routing';
 
 @NgModule({
@@ -15,7 +17,8 @@ import {routes} from './app.routing';
         SIDEDRAWER_DIRECTIVES,
         LISTVIEW_DIRECTIVES,
         DrawerComponent,
-        HomeComponent 
+        HomeComponent,
+        AboutComponent
     ],
     bootstrap: [AppComponent],
     imports: [
--- mzm-mobile/app/app.routing.ts
+++ mzm-mobile/app/app.routing.ts
@@ -2,10 +2,12 @@ import { Router, RouterModule } from '@angular/router';
 
 import {DrawerComponent} from "./pages/drawer/drawer.component";
 import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
 import {AppComponent} from "./app.component";
 
 
 export const routes = RouterModule.forRoot([
-  { path: "", component: AppComponent },
-  { path: "home", component: HomeComponent }
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent }
 ])
